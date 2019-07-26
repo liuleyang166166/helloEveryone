e605e147995900e9421bcbec4ff26f1d83c2cc5c
Daniel <939774887@qq.com>
2019-07-26 21:07:52 +0800

    大强疯了,没得办法

--- src/helloEveryone/PixiJianfa.java
+++ src/helloEveryone/PixiJianfa.java
@@ -3,5 +3,11 @@ package helloEveryone;
 public class PixiJianfa {
 	public static void main(String[] args) {
 		System.out.println("大强想练辟邪剑法!");
+		System.out.println("大强想练辟邪剑法!");
+		System.out.println("大强想练辟邪剑法!");
+		System.out.println("大强想练辟邪剑法!");
+		System.out.println("大强想练辟邪剑法!");
+		DaQiang daQiang = new DaQiang();
+		daQiang.setGender("半男不女!!");
 	}
 }
